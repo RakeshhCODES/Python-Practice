ea0ef2938e926d2f94bd6459763dd4a4a77eae73
Rakesh Mohapatra <158257312+RakeshhCODES@users.noreply.github.com>
2026-05-20 20:39:09 +0530

    Basics MadLibs game using Python

---
+++ P01_Madlibs_Game.py
@@ -0,0 +1,13 @@
+#Project No.01 - MadLibs Game
+
+adjective1 = input("Enter an adjective(description): ")
+noun1 = input("Enter a noun (person, place, thing): ")
+noun2 = input("Enter a noun (person, place, thing): ")
+adjective2 = input("Enter an adjective(description): ")
+verb1 = input("Enter a verb ending with 'ing': ")
+adjective3 = input("Enter an adjective(description): ")
+
+print(f"Shelby went to a {adjective1} park")
+print(f"In the park , Rohan saw {noun1} and {noun2}")
+print(f"{noun1} and {noun2} were {adjective2} and they were {verb1}")
+print(f"Shelby was {adjective3}")
