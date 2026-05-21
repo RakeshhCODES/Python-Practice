ae80a068d9454788f0d448cbba51db0a277e00c2
Rakesh Mohapatra <158257312+RakeshhCODES@users.noreply.github.com>
2026-05-21 12:54:17 +0530

    Circumference calculation program in Python

---
+++ Q03_Circumference_of_circle.py
@@ -0,0 +1,7 @@
+#Circumference of a circle
+
+import math
+radius = float(input("Enter radius of circle: "))
+circumference = 2 * math.pi * radius
+print("Circumference of a circle = 2 * pi * radius")
+print(f"circumference of the circle is: {round(circumference,2)}cm")
