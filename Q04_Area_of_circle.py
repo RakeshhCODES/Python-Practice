380a50f8cf970abb0b626ae8a9e53fd8d6d79639
Rakesh Mohapatra <158257312+RakeshhCODES@users.noreply.github.com>
2026-05-21 20:51:14 +0530

    Area of circle using Python

---
+++ Q04_Area_of_circle.py
@@ -0,0 +1,8 @@
+#Area of a circle
+
+import math
+radius = float(input("Enter radius of circle: "))
+area = math.pi * radius**2
+#area = mat.pi * pow(radius, 2)  --Using POWER Function
+print("Area of circle : math.pi * radius**2")
+print(f"Area of the circle is: {round(area ,2)}cm²")
