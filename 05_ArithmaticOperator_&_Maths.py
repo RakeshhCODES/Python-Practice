cf03b5713d919a671f138d6767c8e1c2e63922a0
Rakesh Mohapatra <158257312+RakeshhCODES@users.noreply.github.com>
2026-05-21 12:44:28 +0530

    Practiced  arithmetic and math operation programs in Python

---
+++ 05_ArithmaticOperator_&_Maths.py
@@ -0,0 +1,40 @@
+#Arithmatic Operators and Maths 
+a = 10
+b = 5   
+print("Addition: ",a+b)         # Addition
+print("Subtraction: ", a - b)     # Subtraction
+print("Multiplication: ", a * b)  # Multiplication
+print("Division: ",a/b)          # Division
+print("Floor Division: ",a//b)  # Floor Division
+print("Modulus: ",a%b)          # Modulus
+print("Exponentiation: ",a**b) #Exponentiation
+print("Square Root: ",a**0.5) #Square Root
+
+
+
+#Math Functions
+a = 16.28
+b = -10.1602
+c = 9
+result = round(a)  #Rounding to nearest integer
+print("Rounded Value: ",result) 
+
+result = abs(b) # Absolute value
+print("Absolute Value: ",result)
+
+result = pow(c, 3) # Power function
+print("Powered Value: ",result)
+
+result = max(a, b, c) # Maximum value
+print("Maximum Value: ",result)
+
+result = min(a, b, c) # Minimum value
+print("Minimum Value: ",result)
+
+
+#Math Module
+import math
+print("Value of pi is:",math.pi) # Value of pi
+print("Value of e is:",math.e) # Value of e
+print("Square root of 25 is:",math.sqrt(25)) # Square root
+print("Ceiling of 4.2 is:",math.ceil(4.2)) # Ceiling
